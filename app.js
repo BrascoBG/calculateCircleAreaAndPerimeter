e5ebe818bc979d990d7d71ca14476493edacaded
Ivo Krastev <52259854+BrascoBG@users.noreply.github.com>
2020-01-10 14:54:44 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+function calculateCircleAreaAndPerimeter([arg1]) {
+ let r = parseInt(arg1);
+ console.log("Area = " + Math.PI * r * r);
+ console.log("Perimeter = " + 2 * Math.PI * r);
+}
+
+calculateCircleAreaAndPerimeter([2]);
